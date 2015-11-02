bf88ca2131fcd80f4833296739b5d05d94913b15
Slava Krauchanka <s.krauchanka@drooms.com>
2015-11-02 09:30:08 +0100

    adding README and some clean-up

--- Data/Aeson/LinkedData.hs
+++ Data/Aeson/LinkedData.hs
@@ -3,5 +3,5 @@ module Data.Aeson.LinkedData where
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as L
 
-decode :: (A.FromJSON a) => L.ByteString -> Maybe a
+decode :: A.FromJSON a => L.ByteString -> Maybe a
 decode = A.decode
---
+++ README.md
@@ -0,0 +1,3 @@
+# AESON-LD: JSON-LD for Haskell
+
+This is an extension to aeson Haskell library to handle JSON-LD data.
